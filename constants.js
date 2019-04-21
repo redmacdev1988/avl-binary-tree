2c3923c57b735259d02d976cf91a616b82ccc512
Ricky Tsao <rtsao6680@gmail.com>
2019-04-21 14:32:56 +0800

    jest tests

--- constants.js
+++ constants.js
@@ -1,12 +1,11 @@
-
-
 module.exports = {
-  PREORDER: 1,
-  INORDER: 2,
-  POSTORDER: 3,
-  LEFT_HEAVY: 4,
-  RIGHT_HEAVY: 5,
-  LEFT: 6,
-  RIGHT: 7,
-  REVERSE_PRINT: 8,
-};
+    PREORDER: 1,
+    INORDER: 2,
+    POSTORDER: 3,
+    LEFT_HEAVY: 4,
+    RIGHT_HEAVY: 5,
+    LEFT: 6,
+    RIGHT: 7,
+    REVERSE_PRINT: 8,
+  };
+  
